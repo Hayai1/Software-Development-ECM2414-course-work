d63b4ba2cfc16c2f9f12f0b36600c38cd8e48fa7
dylan hough <dylanpjhough@gmail.com>
2024-11-24 16:12:24 +0000

    changed maven type

--- game/src/Main.java
+++ game/src/Main.java
@@ -1,7 +1,6 @@
-package com.dh.coursework;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("hi :3");
     }
 }
