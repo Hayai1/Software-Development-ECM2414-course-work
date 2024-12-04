348e7043a2e1f310b0aa4c092929dfec55ea8322
Dylan <dylanpjhough@gmail.com>
2024-12-04 21:19:44 +0000

    updated deck

--- game/src/Deck.java
+++ game/src/Deck.java
@@ -1,3 +1,7 @@
 public class Deck {
-    
+    public Deck(int ID, int maxCards) {}
+    public int getID() { return 0; }
+    public int count() { return 0; }
+    public void addCard(Card card) {}
+    public Card getCard() { return new Card(100000); }
 }
